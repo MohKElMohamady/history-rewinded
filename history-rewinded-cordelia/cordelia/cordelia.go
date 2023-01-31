b05bb63acd6c0ba8dac24085497b4467202661d5
mohkelmohamady <moh.k.elmohamady@gmail.com>
2023-01-31 23:14:57 +0100

    added the method to cordelia struct to fetch concurrently the events, births, deaths and holidays on a day

--- history-rewinded-cordelia/cordelia/cordelia.go
+++ history-rewinded-cordelia/cordelia/cordelia.go
@@ -5,15 +5,17 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/dghubble/oauth1"
+	"google.golang.org/grpc"
 	"history-rewinded-cordelia/twitter"
 	"history-rewinded-regan/pb"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/dghubble/oauth1"
-	"google.golang.org/grpc"
+	"sync"
+	"time"
 )
 
 const kingLearAddress = ":1606"
@@ -28,7 +30,7 @@ type Tweeter interface {
 }
 
 type cordelia struct {
-	http    *http.Client
+	http      *http.Client
 	king_lear pb.LearClient
 }
 
@@ -51,7 +53,6 @@ func New() (cordelia, error) {
 func (c *cordelia) Tweet() {
 	fmt.Println("Tweeting, tweet tweet")
 }
-
 
 func (c *cordelia) TweetEvent(incident *pb.Incident) twitter.Tweet {
 	tweetReqest := twitter.IncidentToTweetRequest(incident)
@@ -76,6 +77,125 @@ func (c *cordelia) TweetEvent(incident *pb.Incident) twitter.Tweet {
 	}
 
 	//TODO: Save the successfull tweet with its timestamp in Cassandra
-	log.Println("successfully tweeted the event, the tweet id is %d and the its text is %s", tweet.TweetId, tweet.Text)
-	return tweet 
+	log.Printf("successfully tweeted the event, the tweet id is %d and the its text is %s", tweet.TweetId, tweet.Text)
+	return tweet
+}
+
+func (c *cordelia) FetchIncidentaOfTheDay() (events []pb.Incident, births []pb.Incident, deaths []pb.Incident, holidays []pb.Incident) {
+
+	today := int64(time.Now().Day())
+	currentMonth := int64(time.Now().Month())
+
+	fourIncidentsTypeSynchronizer := sync.WaitGroup{}
+
+	fourIncidentsTypeSynchronizer.Add(4)
+	events = []pb.Incident{}
+	births = []pb.Incident{}
+	deaths = []pb.Incident{}
+	holidays = []pb.Incident{}
+
+	eventsChannel := c.FetchEventsOn(today, currentMonth, pb.IncidentType_INCIDENT_TYPE_EVENT)
+	birthsChannel := c.FetchEventsOn(today, currentMonth, pb.IncidentType_INCIDENT_TYPE_EVENT)
+	deathsChannel := c.FetchEventsOn(today, currentMonth, pb.IncidentType_INCIDENT_TYPE_EVENT)
+	holidaysChannel := c.FetchEventsOn(today, currentMonth, pb.IncidentType_INCIDENT_TYPE_EVENT)
+
+	go func() {
+		for e := range eventsChannel {
+			events = append(events, e)
+		}
+		fourIncidentsTypeSynchronizer.Done()
+	}()
+
+	go func() {
+		for b := range birthsChannel {
+			events = append(events, b)
+		}
+		fourIncidentsTypeSynchronizer.Done()
+	}()
+
+	go func() {
+		for d := range deathsChannel {
+			events = append(events, d)
+		}
+		fourIncidentsTypeSynchronizer.Done()
+	}()
+
+	go func() {
+		for h := range holidaysChannel {
+			events = append(events, h)
+		}
+		fourIncidentsTypeSynchronizer.Done()
+	}()
+
+	fourIncidentsTypeSynchronizer.Wait()
+	return events, births, deaths, holidays
+}
+
+func (c *cordelia) FetchEventsOn(day int64, month int64, incidentType pb.IncidentType) <-chan pb.Incident {
+
+	EventsIncidents := make(chan pb.Incident)
+
+	req := pb.FetchIncidentRequest{
+		Day:   day,
+		Month: month,
+	}
+	go func() {
+		switch incidentType {
+		case pb.IncidentType_INCIDENT_TYPE_EVENT:
+			client, err := c.king_lear.FetchEventsOn(context.Background(), &req)
+			log.Fatalf("failed to invoke the rpc for fetching events, reason:%s\n", err.Error())
+			for {
+				incident, err := client.Recv()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					log.Fatalf("failed to continue receive incidents from stream, reason:%s\n", err.Error())
+				}
+				EventsIncidents <- *incident
+			}
+		case pb.IncidentType_INCIDENT_TYPE_BIRTH:
+			client, err := c.king_lear.FetchBirthsOn(context.Background(), &req)
+			log.Fatalf("failed to invoke the rpc for fetching births, reason:%s\n", err.Error())
+			for {
+				incident, err := client.Recv()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					log.Fatalf("failed to continue receive births from stream, reason:%s\n", err.Error())
+				}
+				EventsIncidents <- *incident
+			}
+		case pb.IncidentType_INCIDENT_TYPE_DEATH:
+			client, err := c.king_lear.FetchEventsOn(context.Background(), &req)
+			log.Fatalf("failed to invoke the rpc for fetching deaths, reason:%s\n", err.Error())
+			for {
+				incident, err := client.Recv()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					log.Fatalf("failed to continue receive deaths from stream, reason:%s\n", err.Error())
+				}
+				EventsIncidents <- *incident
+			}
+		case pb.IncidentType_INCIDENT_TYPE_HOLIDAY:
+			client, err := c.king_lear.FetchEventsOn(context.Background(), &req)
+			log.Fatalf("failed to invoke the rpc for fetching holidays, reason:%s\n", err.Error())
+			for {
+				incident, err := client.Recv()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					log.Fatalf("failed to continue receive holidays from stream, reason:%s\n", err.Error())
+				}
+				EventsIncidents <- *incident
+			}
+
+		}
+	}()
+
+	return EventsIncidents
 }

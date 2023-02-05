7fef7b3760ac592b521d6cb87b08e919c426064c
mohkelmohamady <moh.k.elmohamady@gmail.com>
2023-02-05 23:51:47 +0100

    Added the implementation of registering users for CassandraClient

--- history-rewinded-goneril/db/cassandra.go
+++ history-rewinded-goneril/db/cassandra.go
@@ -3,10 +3,12 @@ package db
 import (
 	"context"
 	"crypto/tls"
+	"errors"
 	"history-rewinded-goneril/models"
 	"history-rewinded-regan/pb"
 	"log"
 	"os"
+	"strings"
 	"sync"
 
 	"github.com/google/uuid"
@@ -14,6 +16,7 @@ import (
 	"github.com/stargate/stargate-grpc-go-client/stargate/pkg/auth"
 	"github.com/stargate/stargate-grpc-go-client/stargate/pkg/client"
 	"github.com/stargate/stargate-grpc-go-client/stargate/pkg/proto"
+	"golang.org/x/crypto/bcrypt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
@@ -21,7 +24,7 @@ import (
 var remoteCassandraUri string
 var remoteCassandraBearerToken string
 var allocatedConnectionsToCassandra int64
-var cassandraClientsPool = sync.Pool{
+var cassandraConnectionsPool = sync.Pool{
 	New: func() any {
 		config := &tls.Config{InsecureSkipVerify: false}
 		conn, err := grpc.Dial(
@@ -52,19 +55,19 @@ func init() {
 	remoteCassandraUri = os.Getenv("CASSANDRA_REMOTE_URI")
 	remoteCassandraBearerToken = os.Getenv("CASSANDRA_BEARER_TOKEN")
 
-	client1 := cassandraClientsPool.Get().(client.StargateClient)
-	defer cassandraClientsPool.Put(client1)
+	client1 := cassandraConnectionsPool.Get().(*client.StargateClient)
+	defer cassandraConnectionsPool.Put(client1)
 	go func() {
 		client1.ExecuteQueryWithContext(
 			&proto.Query{Cql: `CREATE TABLE IF NOT EXISTS goneril.users 
-								(email text, username text, password text, birthday date) 
-								PRIMARY KEY ((email, username))`},
+								(email text, password text, birthday date) 
+								PRIMARY KEY (email)`},
 			context.Background(),
 		)
 	}()
 
-	client2 := cassandraClientsPool.Get().(client.StargateClient)
-	defer cassandraClientsPool.Put(client2)
+	client2 := cassandraConnectionsPool.Get().(*client.StargateClient)
+	defer cassandraConnectionsPool.Put(client2)
 	go func() {
 		client2.ExecuteQueryWithContext(
 			&proto.Query{Cql: `CREATE TABLE IF NOT EXISTS goneril.favorites_incidents 
@@ -77,9 +80,8 @@ func init() {
 }
 
 type CassandraRepository interface {
-	RegisterUser(username, password string) error
-	LoginWithEmail(email, password string) models.User
-	LoginWithUsername(username, password string) models.User
+	RegisterUser(email , password string, birthdate uint32) error
+	Login(email, password string) models.User
 	ResetPassword(email string) error
 	AddFavoriteIncident(pb.Incident) error
 	DeleteIncidentFromFavorites(id uuid.UUID) error
@@ -93,9 +95,49 @@ type CassandraRepository interface {
 type CassandraClient struct {
 }
 
-func (c CassandraClient) RegisterUser(username, password string) error {
-	//TODO implement me
-	panic("implement me")
+func (c CassandraClient) RegisterUser(username, password string, birthdate uint32) error {
+	client := cassandraConnectionsPool.Get().(*client.StargateClient)	
+	res, err := client.ExecuteQueryWithContext(&proto.Query{
+		Cql: "SELECT * FROM goneril.users WHERE email = ?;",
+		Values: &proto.Values{Values: []*proto.Value{{Inner: &proto.Value_String_{String_: username}}}},
+	},context.Background())
+	if err != nil {
+		return errors.New("unable to fetch users with the same name to check if it unique")
+	}
+
+	if len(res.GetResultSet().Rows) != 0 {
+		return errors.New("the email already exists and the user is already registered")
+	}
+
+	passwordReader := strings.NewReader(password)
+	passwordInBytes := make([]byte, 32)
+	_, err = passwordReader.Read(passwordInBytes)
+	if err != nil {
+		return errors.New("failed to read into the bytes buffer and parse the password to bytes")
+	}
+
+	hashedPasswordInBytes, err := bcrypt.GenerateFromPassword(passwordInBytes, 8)
+	if err != nil {
+		return errors.New("unabled to hash password")
+	}
+
+	hashedPassword := string(hashedPasswordInBytes)
+
+	res, err = client.ExecuteQuery(&proto.Query{
+		Cql: `INSERT INTO goneril.user (email, password, birthdate) VALUES (?, ?, ?);`,
+		Values: &proto.Values{
+			Values: []*proto.Value{
+				{Inner: &proto.Value_String_{username}},
+				{Inner: &proto.Value_String_{hashedPassword}},
+				{Inner: &proto.Value_Date{Date: birthdate}},
+			},
+		},
+	})
+	if err != nil {
+		return errors.New("unable to insert the new user in the table")
+	}
+
+	return nil
 }
 
 func (c CassandraClient) LoginWithEmail(email, password string) models.User {
